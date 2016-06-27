78a50bcec61a7668d66cae88c3812d1f1a114cd6
Fraser Pollock <fpollock@chef.io>
2016-06-27 12:01:00 -0400

    Initial commit of secondary apache configuration

---
+++ cookbooks/site-config/metadata.rb
@@ -0,0 +1,7 @@
+name 'site-config'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures site-config'
+long_description 'Installs/Configures site-config'
+version '0.1.0'
---
+++ cookbooks/site-config/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: site-config
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
