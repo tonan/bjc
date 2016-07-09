7a5c68015206ab1cd695a6c5801c707134cd4384
Bakh Inamov <b@chef.io>
2016-07-08 20:17:01 -0700

    restores backup data into delivery

--- cookbooks/bjc-delivery/metadata.rb
+++ cookbooks/bjc-delivery/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures bjc-delivery'
 long_description 'Installs/Configures bjc-delivery'
-version '0.1.0'
+version '0.1.1'
 
 depends 'wombat'
 # depends 'delivery'
