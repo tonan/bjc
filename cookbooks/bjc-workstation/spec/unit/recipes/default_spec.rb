fc43e4c09ce068b10cf77f711ce20271b542e17a
Bakh Inamov <b@chef.io>
2016-07-10 01:34:06 -0700

    specifies platform in spec file

--- cookbooks/bjc-workstation/spec/unit/recipes/default_spec.rb
+++ cookbooks/bjc-workstation/spec/unit/recipes/default_spec.rb
@@ -9,7 +9,7 @@ require 'spec_helper'
 describe 'bjc-workstation::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2012R2')
       runner.converge(described_recipe)
     end
 
