4467617858ce603212b9884d57e91da3d6a1dd44
Bakh Inamov <b@chef.io>
2016-08-16 14:48:03 -0700

    0.0.18 w hosts files in compliance, chef and automate, fixes #61
    new template with all them latest AMIs

--- cookbooks/bjc-chef-server/recipes/default.rb
+++ cookbooks/bjc-chef-server/recipes/default.rb
@@ -4,8 +4,5 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-node.normal['demo']['build-nodes'] = 3
-# this value wasn't taking from wombat.lock
-
 include_recipe 'apt'
 include_recipe 'chef-server'
