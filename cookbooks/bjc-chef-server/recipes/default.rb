a4e8e1dc89589377b815bfe54617cb772afa26fa
Bakh Inamov <b@chef.io>
2016-08-16 23:18:51 -0700

    0.0.20 ingests chef-server data into viz, connects to #33
    there’s some scaffolding for creating AURD environments, but it doesn’t do anything yet, shipping as a “silent feature”

--- cookbooks/bjc-chef-server/recipes/default.rb
+++ cookbooks/bjc-chef-server/recipes/default.rb
@@ -6,3 +6,14 @@
 
 include_recipe 'apt'
 include_recipe 'chef-server'
+
+chef_ingredient 'chef-server' do
+  action :reconfigure
+  config <<-EOS
+
+data_collector['root_url'] = 'https://delivery.#{node['demo']['domain']}/data-collector/v0/'
+data_collector['token'] = '93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506'
+EOS
+end
+
+include_recipe 'bjc-chef-server::cheffish'
