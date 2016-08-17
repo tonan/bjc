50d4a7dde8bf7157d720d3edf7603b5baa83f82e
Sean Carolan <scarolan@gmail.com>
2016-08-17 13:53:21 -0700

    Fixed sql restore script, bumped cookbook version, updated wombat.lock, cut new AMIs and JSON template.

--- cookbooks/bjc-compliance/recipes/restore_backup.rb
+++ cookbooks/bjc-compliance/recipes/restore_backup.rb
@@ -6,17 +6,18 @@
 
 # Restores Chef Compliance data from a backup created with pg_dump.
 # You can use this command to create the backup file:
-# sudo -u chef-pgsql /opt/chef-compliance/embedded/bin/pg_dump chef_compliance > compliance_backup.sql
+# sudo -u chef-pgsql /opt/chef-compliance/embedded/bin/pg_dump -c chef_compliance > compliance_backup.sql
 
 # Drop your backup file into the Chef file cache directory:
 cookbook_file "#{Chef::Config[:file_cache_path]}/compliance_backup.sql" do
   action :create
   source 'compliance_backup.sql'
+  sensitive true
 end
 
 # And run the psql command to restore the data
 execute 'Restore Chef Compliance Backup' do
-  command "/opt/chef-compliance/embedded/bin/psql chef_compliance < #{Chef::Config[:file_cache_path]}/compliance_backup.sql"
+  command "/opt/chef-compliance/embedded/bin/psql -q chef_compliance < #{Chef::Config[:file_cache_path]}/compliance_backup.sql"
   user 'chef-pgsql'
   sensitive true
   # TO DO: write a guard for this
