a7e169793fcf89e666f8831ec850a4b782b0b4eb
Bakh Inamov <b@chef.io>
2016-08-18 11:55:32 -0700

    bjc-delivery 0.1.2 now builds post wombat renames

--- cookbooks/bjc-delivery/test/fixtures/cookbooks/test/recipes/default.rb
+++ cookbooks/bjc-delivery/test/fixtures/cookbooks/test/recipes/default.rb
@@ -4,9 +4,9 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-%w(chef-server.crt
-   delivery.crt
-   delivery.key
+%w(chef.crt
+   automate.crt
+   automate.key
    compliance.crt
    public.pub
    private.pem
