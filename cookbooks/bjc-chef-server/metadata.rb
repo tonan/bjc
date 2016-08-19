edeb5e47e7f2bbfc71844de63020cf905fd2f90a
Bakh Inamov <b@chef.io>
2016-08-18 20:51:43 -0700

    0.1.2 fixes data collector for automate/viz

--- cookbooks/bjc-chef-server/metadata.rb
+++ cookbooks/bjc-chef-server/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures bjc-chef-server'
 long_description 'Installs/Configures bjc-chef-server'
-version '0.1.1'
+version '0.1.2'
 
 depends 'chef_server'
 depends 'apt'
