d3e75e99ce944d0a56e9dc1b6edcbbf01b484f70
Sean Carolan <scarolan@gmail.com>
2016-08-24 21:32:02 -0500

    Fixed environment.rb recipe.

--- cookbooks/bjc-workstation/recipes/environment.rb
+++ cookbooks/bjc-workstation/recipes/environment.rb
@@ -4,9 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+# NOTE:  This recipe did not work with Test Kitchen. 
+# It is included in the packer/bjc-workstation.json run_list though.
+
+home = Dir.home
+
 # Do the needful
+# We need to drop this PuTTY-formatted SSH key in ~/.ssh so PuTTY will work.
 file "#{home}/.ssh/id_rsa.ppk" do
-  # This won't work with Test Kitchen, we use a fixture cookbook for TK instead
   content lazy { IO.read("C:/Windows/Temp/putty.ppk") }
   action :create
 end
