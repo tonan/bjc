4ba231d5ab45d8199d670d9a73daac1d664730cb
Sean Carolan <scarolan@gmail.com>
2016-08-26 12:19:23 -0500

    Moved compliance profile import into bash script, which must be called via the demo startup script.  See https://github.com/chef/chef-compliance/issues/863 for more details on why...

--- cookbooks/bjc-compliance/recipes/load_apache_profile.rb
+++ cookbooks/bjc-compliance/recipes/load_apache_profile.rb
@@ -11,13 +11,14 @@ cookbook_file src_filepath do
   source src_filename
 end
 
-execute 'upload_profile' do
-  cwd ::File.dirname(src_filepath)
-  command <<-EOH
-  export API_URL="https://localhost/api"
-  export API_TOKEN=$(curl -X POST $API_URL/login -k -s -d '{"userid": "workstation-1", "password": "workstation!"}')
-  export AUTH="Authorization: Bearer $API_TOKEN"
-  curl -X POST "$API_URL/owners/admin/compliance/apache_webserver/tar" -k -H "$AUTH" --data-binary "@#{src_filepath}"
-  EOH
-  #not_if { ::File.exist?(src_filepath) }
+cookbook_file "#{Chef::Config['file_cache_path']}/upload_apache_profile.sh" do
+  action :create
+  source 'upload_apache_profile.sh'
+  mode '0755'
 end
+
+# This doesn't work. You MUST run this script manually or via SSH
+#bash "Import Apache Compliance profile" do
+#  code "#{Chef::Config['file_cache_path']}/upload_apache_profile.sh"
+#  action :run
+#end
