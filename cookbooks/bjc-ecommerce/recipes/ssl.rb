245156359cf42ab74c3cd3c882b5084c61881623
Sean Carolan <scarolan@gmail.com>
2016-09-06 18:16:09 -0500

    Adds SSL support to our shopping cart.

---
+++ cookbooks/bjc-ecommerce/recipes/ssl.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: bjc-ecommerce
+# Recipe:: ssl
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+# Enables SSL and mod_jk
+
+package 'apache2'
+
+package 'libapache2-mod-jk'
+
+execute 'a2enmod jk' 
+
+execute 'a2enmod ssl'
+
+execute 'a2ensite default-ssl'
+
+service 'tomcat7' do
+  action :nothing
+end
+
+service 'apache2' do
+  action :nothing
+end
+
+template '/etc/apache2/sites-available/default-ssl.conf' do
+  action :create
+  source 'default-ssl.conf.erb'
+  notifies :restart, 'service[apache2]'
+end
+
+template '/etc/libapache2-mod-jk/workers.properties' do
+  action :create
+  source 'workers.properties.erb'
+  notifies :restart, 'service[apache2]'
+end
+
+template '/etc/tomcat7/server.xml' do
+  action :create
+  source 'server.xml.erb'
+  notifies :restart, 'service[tomcat7]'
+end
+
