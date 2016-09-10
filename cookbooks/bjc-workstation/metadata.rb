459723ebaeb60efe94695262f33ca2db29a7c111
Sean Carolan <scarolan@gmail.com>
2016-09-09 21:53:24 -0500

    Added bookmarks for demo.

--- cookbooks/bjc-workstation/metadata.rb
+++ cookbooks/bjc-workstation/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures bjc-workstation'
 long_description 'Installs/Configures bjc-workstation'
-version '0.1.9'
+version '0.1.10'
 
 depends 'workstation'
 depends 'windows'
