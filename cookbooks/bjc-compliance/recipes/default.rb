ba560c1a451e4704bc2c8f941f235154035736f0
Bakh Inamov <b@chef.io>
2016-09-12 12:35:18 -0700

    0.1.5 sets chef_gate_compliance secret

--- cookbooks/bjc-compliance/recipes/default.rb
+++ cookbooks/bjc-compliance/recipes/default.rb
@@ -8,3 +8,9 @@ include_recipe 'compliance'
 include_recipe 'bjc-compliance::restore_backup'
 include_recipe 'bjc-compliance::load_apache_profile'
 include_recipe 'bjc-compliance::load_ssl_profile'
+
+#SETS CHEF_GATE_COMPLIANCE_SECRET TO MATCH THE ONE ON THE CHEF SERVER
+file '/opt/chef-compliance/sv/core/env/CHEF_GATE_COMPLIANCE_SECRET' do
+	content lazy { IO.read('/tmp/public.pub') }
+	sensitive true
+end
