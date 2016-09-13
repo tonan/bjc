da4aaf7e09e3346f9f5bd932ffe57498739a7674
Sean Carolan <scarolan@gmail.com>
2016-09-13 11:10:38 -0700

    Updated infranodes to start chef-client upon boot.

--- cookbooks/bjc_infranodes/metadata.rb
+++ cookbooks/bjc_infranodes/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures bjc_infranodes'
 long_description 'Installs/Configures bjc_infranodes'
-version '0.1.0'
+version '0.1.1'
 
 depends 'infranodes'
