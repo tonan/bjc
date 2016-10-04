06aaf3c396a11e7a58098595e2ef7540caf597a2
Sean Carolan <scarolan@gmail.com>
2016-10-04 16:49:58 -0500

    New - with more compliance profiles.

--- cookbooks/bjc-compliance/metadata.rb
+++ cookbooks/bjc-compliance/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures bjc-compliance'
 long_description 'Installs/Configures bjc-compliance'
-version '0.1.5'
+version '0.1.6'
 
 depends 'compliance'
 depends 'audit'
