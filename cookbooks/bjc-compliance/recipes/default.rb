dbb0c855d01a4dd8862e19d0ed97eef557282147
Sean Carolan <scarolan@gmail.com>
2016-10-04 16:40:17 -0500

    Refactored profile upload into one script.

--- cookbooks/bjc-compliance/recipes/default.rb
+++ cookbooks/bjc-compliance/recipes/default.rb
@@ -8,8 +8,6 @@ include_recipe 'compliance'
 include_recipe 'audit'
 include_recipe 'bjc-compliance::restore_backup'
 include_recipe 'bjc-compliance::load_profiles'
-#include_recipe 'bjc-compliance::load_apache_profile'
-#include_recipe 'bjc-compliance::load_ssl_profile'
 
 #SETS CHEF_GATE_COMPLIANCE_SECRET TO MATCH THE ONE ON THE CHEF SERVER
 file '/opt/chef-compliance/sv/core/env/CHEF_GATE_COMPLIANCE_SECRET' do
