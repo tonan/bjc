138e32b086ad82146e6f60c93582b06181817d02
Bakh Inamov <b@chef.io>
2016-10-15 17:53:09 -0700

    0.1.17 adds bjc_bass to workstation:~/cookbooks/

--- cookbooks/bjc_workstation/attributes/default.rb
+++ cookbooks/bjc_workstation/attributes/default.rb
@@ -1,1 +1,1 @@
-default['bjc_workstation']['cookbooks'] = ['bass_web','site-config','bjc-ecommerce']
+default['bjc_workstation']['cookbooks'] = ['bass_web','site-config','bjc-ecommerce', 'bjc_bass']
