984401e4e42c0fea21b1534d8f5fd803617833a6
Sean Carolan <scarolan@gmail.com>
2016-10-21 19:53:29 -0500

    Initial stab at getting clean ChefSpec run on a cookbook.

--- cookbooks/bjc_chef_server/spec/unit/recipes/default_spec.rb
+++ cookbooks/bjc_chef_server/spec/unit/recipes/default_spec.rb
@@ -9,7 +9,8 @@ require 'spec_helper'
 describe 'bjc_chef_server::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04')
+      # runner = ChefSpec::ServerRunner.new
       runner.converge(described_recipe)
     end
 
