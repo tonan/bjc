e58606dbeb57398110b42c3d0470d1f940fac9cf
Sean Carolan <scarolan@gmail.com>
2016-10-24 08:44:43 -0500

    Unit tests for bjc_infranodes

--- cookbooks/bjc_infranodes/spec/unit/recipes/default_spec.rb
+++ cookbooks/bjc_infranodes/spec/unit/recipes/default_spec.rb
@@ -13,6 +13,19 @@ describe 'bjc_infranodes::default' do
       runner.converge(described_recipe)
     end
 
+    it 'includes the infranodes recipe' do
+      expect(chef_run).to include_recipe('infranodes')
+    end
+
+    it 'renders the /etc/rc.local template' do
+      expect(chef_run).to render_file('/etc/rc.local').with_content('Waiting for Chef Server to be available')
+    end
+
+    it 'includes the wombat authorized-keys and etc-hosts recipes' do
+      expect(chef_run).to include_recipe('wombat::authorized-keys')
+      expect(chef_run).to include_recipe('wombat::etc-hosts')
+    end
+
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
