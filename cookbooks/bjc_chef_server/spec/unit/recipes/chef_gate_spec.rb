750600ee6e8c53214a8e3c2618e23d3f21cd59fb
Sean Carolan <scarolan@gmail.com>
2016-10-24 10:20:15 -0500

    Finished unit tests for bjc_chef_server recipes.

--- cookbooks/bjc_chef_server/spec/unit/recipes/chef_gate_spec.rb
+++ cookbooks/bjc_chef_server/spec/unit/recipes/chef_gate_spec.rb
@@ -8,11 +8,24 @@ require 'spec_helper'
 
 describe 'bjc_chef_server::chef_gate' do
   context 'When all attributes are default, on an unspecified platform' do
-
     let(:chef_run) do
-      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '14.04')
-      # runner = ChefSpec::ServerRunner.new
-      runner.converge(described_recipe)
+      ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '14.04') do |node|
+        #node.normal['bjc_workstation']['cookbooks'] = ['bass_web', 'site-config', 'bjc-ecommerce', 'bjc_bass']
+				node.normal['ipaddress'] = '192.168.13.37'
+			  node.normal['demo']['domain'] = 'chef-demo.com'
+      end.converge(described_recipe)
+    end
+
+    it 'stops the omnibus service' do
+      expect(chef_run).to stop_omnibus_service('chef-server/nginx')
+    end
+
+    it 'runs the bash command to stop chef-gate' do
+      expect(chef_run).to run_bash('stop chef-gate')
+    end
+
+    it 'deletes the temporary hostfile entry' do
+      expect(chef_run).to edit_delete_lines('Remove temporary hostfile entry we added earlier')
     end
 
     it 'converges successfully' do
