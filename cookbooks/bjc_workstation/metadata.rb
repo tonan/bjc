5f3b41549f1f82768f61760c1621001712195c48
Sean Carolan <scarolan@gmail.com>
2016-10-27 11:19:16 -0700

    Moves like Jagger.

--- cookbooks/bjc_workstation/metadata.rb
+++ cookbooks/bjc_workstation/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc_workstation'
 long_description 'Installs/Configures bjc_workstation'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.24'
+version '0.1.25'
 
 depends 'workstation'
 depends 'windows'
