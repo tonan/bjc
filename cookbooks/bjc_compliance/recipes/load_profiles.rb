1a091c0be42d62c9e5db188092b22355b5a85d21
Sean Carolan <scarolan@gmail.com>
2016-10-31 13:53:03 -0500

    Fixed compliance profiles.

--- cookbooks/bjc_compliance/recipes/load_profiles.rb
+++ cookbooks/bjc_compliance/recipes/load_profiles.rb
@@ -9,7 +9,7 @@ profiles = %w(
   cis-apachetomcat-5.5-6.0-level1.zip
   cis-apachetomcat-5.5-6.0-level2.zip
   cis-microsoftwindows2012r2-level1-memberserver.tar.gz
-  ssl-benchmark.tar.gz
+  ssl-benchmark.zip
   stig-redhat.zip
 )
 
