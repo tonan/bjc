5526145e78d0d7ffe737ecc851a7de80895540a1
Sean Carolan <scarolan@gmail.com>
2016-12-08 15:49:27 -0600

    Put the build command back.

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -7,7 +7,7 @@
 log node['delivery']['workspace_path']
 
 execute "build-the-things" do
-  command "wombat --version"
+  command "wombat build --parallel"
   cwd "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
   action :run
 end
