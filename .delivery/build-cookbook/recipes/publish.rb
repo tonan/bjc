149328d254a90a34ac0986420a11a53ba8bf8c4d
Sean Carolan <scarolan@gmail.com>
2016-12-08 15:42:35 -0600

    Log workspace path.

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+log node['delivery']['workspace_path']
+
 execute "build-the-things" do
   command "wombat --version"
   cwd node['delivery']['workspace_path']
