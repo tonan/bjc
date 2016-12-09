db8326c526153287ea59d9e4a7801c0cc6cf8540
Sean Carolan <scarolan@gmail.com>
2016-12-09 11:19:44 -0600

    Add key copy execute resource.

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -6,6 +6,14 @@
 
 # log node['delivery']['workspace_path']
 
+# Copy keys into the packer directory
+execute "copy-packer-keys" do
+  command "tar -zxvf /var/opt/delivery/workspace/Downloads/keys.tar.gz -C packer/keys"
+  live_stream true
+  cwd "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
+  action :run
+end
+
 # Build new Chef Demo AMIs and associated Cloudformation Template
 execute "build-the-things" do
   command "/var/opt/delivery/workspace/wombat_build.sh"
