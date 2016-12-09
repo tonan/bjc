9feab82ebd75678cc858e3fd45c48ab4d71578d5
Sean Carolan <scarolan@gmail.com>
2016-12-09 08:43:33 -0600

    You get a rubocop.yml...and you get a rubocop.yml...

--- cookbooks/bjc_chef_server/metadata.rb
+++ cookbooks/bjc_chef_server/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc_chef_server'
 long_description 'Installs/Configures bjc_chef_server'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.13'
+version '0.1.14'
 
 depends 'chef_server'
 depends 'apt'
