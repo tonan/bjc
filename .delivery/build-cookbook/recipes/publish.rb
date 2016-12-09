be70fb5e35b7c4725fed94f5e8006e38b7a9ecf1
Sean Carolan <scarolan@gmail.com>
2016-12-08 19:59:26 -0600

    Added fix to get AWS keys loaded into memory.

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -8,14 +8,14 @@
 
 # Build new Chef Demo AMIs and associated Cloudformation Template
 execute "build-the-things" do
-  command "wombat build --parallel"
+  command "source /var/opt/delivery/workspace/.bashrc; wombat build --parallel"
   live_stream true
   cwd "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
   action :run
 end
 
 execute "generate-json" do
-  command "wombat update"
+  command "source /var/opt/delivery/workspace/.bashrc; wombat update"
   live_stream true
   cwd "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
   action :run
