9eb2efe26df1b84a13816a5a443258872eb2653b
Sean Carolan <scarolan@gmail.com>
2016-12-09 13:57:45 -0600

    Going back forward to 0.3.4

--- cookbooks/bjc_build_node/metadata.rb
+++ cookbooks/bjc_build_node/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc_build_node'
 long_description 'Installs/Configures bjc_build_node'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.8'
+version '0.1.9'
 
 depends 'build_node'
 depends 'wombat'
