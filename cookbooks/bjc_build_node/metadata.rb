8ca240fd034929027ef988d4bd0e4b1ccc4ab1e1
Sean Carolan <scarolan@gmail.com>
2016-12-09 13:55:08 -0600

    Bump bump bump

--- cookbooks/bjc_build_node/metadata.rb
+++ cookbooks/bjc_build_node/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc_build_node'
 long_description 'Installs/Configures bjc_build_node'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.7'
+version '0.1.8'
 
 depends 'build_node'
 depends 'wombat'
