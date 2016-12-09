84cd7f2b9b18b4240a35c1d29719fd1d42a93c5f
Sean Carolan <scarolan@gmail.com>
2016-12-08 20:04:40 -0600

    Trying another way to add env vars.

--- .delivery/build-cookbook/metadata.rb
+++ .delivery/build-cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build-cookbook'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
-version '0.1.4'
+version '0.1.5'
 
 depends 'packer'
 depends 'delivery-truck'
