1b266c9f32424b5497a887381f1b0a9bf7874940
Nick Rycar <rycar@chef.io>
2016-12-14 13:59:32 -0800

    Minor code cleanup. Removes duplicate entries set in the chef_server cookbook already.

--- cookbooks/bjc_chef_server/recipes/chef_gate.rb
+++ cookbooks/bjc_chef_server/recipes/chef_gate.rb
@@ -22,9 +22,6 @@ end
 chef_ingredient 'chef-server' do
   action :reconfigure
   config <<-EOS
-
-data_collector['root_url'] = 'https://#{node['demo']['domain_prefix']}automate.#{node['demo']['domain']}/data-collector/v0/'
-data_collector['token'] = '93a49a4f2482c64126f7b6015e6b0f30284287ee4054ff8807fb63d9cbd1c506'
 oc_id['applications'] ||= {}
 oc_id['applications']['chef_gate'] = {
      'redirect_uri' => 'https://#{node['demo']['domain_prefix']}compliance.#{node['demo']['domain']}/auth/oc_id/callback'
