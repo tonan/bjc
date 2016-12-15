690924931f55a2391a1cb26af1c828907ec9ebf6
Sean Carolan <scarolan@gmail.com>
2016-12-15 14:22:39 -0600

    Added S3 upload code.

--- .delivery/build-cookbook/recipes/publish.rb
+++ .delivery/build-cookbook/recipes/publish.rb
@@ -4,29 +4,36 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-# log node['delivery']['workspace_path']
+require 'aws-sdk'
+
+# This could probably be refactored a bit
+workspace = "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
 
 # Copy keys into the packer directory
 execute "copy-packer-keys" do
   command "tar -zxvf /var/opt/delivery/workspace/Downloads/keys.tar.gz -C packer/keys"
   live_stream true
-  cwd "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
+  cwd workspace
   action :run
 end
 
-# Build new Chef Demo AMIs and associated Cloudformation Template
+# Build new Chef Demo AMIs
 execute "build-the-things" do
   command "/var/opt/delivery/workspace/wombat_build.sh"
   live_stream true
-  cwd "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
+  cwd workspace
   action :run
 end
 
+# Create a new bjc-demo.json template
 execute "generate-json" do
   command "/var/opt/delivery/workspace/wombat_update.sh"
   live_stream true
-  cwd "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/build/publish/repo"
+  cwd workspace
   action :run
 end
 
-# Here is where we will use the Ruby AWS SDK to upload bjc-demo.json to S3
+# Use the Ruby AWS SDK to upload bjc-demo.json to S3
+s3 = Aws::S3::Resource.new(region:'us-west-2')
+obj = s3.bucket('bjc-public/cloudformation').object('key')
+obj.upload_file("#{workspace}/stacks/bjc-demo.json")
