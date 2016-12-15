cac2235699b4c141ab4cdb2f33fad9f6c84a55dc
Sean Carolan <scarolan@gmail.com>
2016-12-15 17:07:46 -0600

    Moved wombat deploy and outputs commands into wrapper script.

--- .delivery/build-cookbook/recipes/deploy.rb
+++ .delivery/build-cookbook/recipes/deploy.rb
@@ -24,20 +24,8 @@ execute 'Copy wombat.lock' do
 end
 
 execute 'Deploy Demo Stack' do
-  command "wombat deploy bjc-demo"
+  command "#{node['delivery']['workspace_path']}/wombat_deploy.sh"
   cwd node['delivery']['workspace_path']
-  live_stream true
-  action :run
-end
-
-ruby_block 'Wait for demo to spin up...' do
-  block do
-    sleep 120
-  end
-end
-
-execute 'Fetch workstation IP' do
-  command 'wombat outputs bjc-demo'
   live_stream true
   action :run
 end
