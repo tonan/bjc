9cbf1e2aa5825a6277dcfdcd54c967e23e98dd6c
Sean Carolan <scarolan@gmail.com>
2016-12-15 18:42:05 -0600

    Run deploy only in acceptance.

--- .delivery/build-cookbook/recipes/deploy.rb
+++ .delivery/build-cookbook/recipes/deploy.rb
@@ -3,6 +3,9 @@
 # Recipe:: deploy
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+return unless ['acceptance'].include?(node['delivery']['change']['stage'])
+
 workspace = "#{node['delivery']['workspace_path']}/bjc-automate-server-5g9aorii6yvcetdi.us-west-2.opsworks-cm.io/default/chef-sas/bjc/master/acceptance/deploy/repo"
 
 # Copy keys into the packer directory
