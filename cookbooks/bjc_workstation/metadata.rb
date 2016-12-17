2b8e5d67a4867b09831943bd51e90a8726f5e0cc
Sean Carolan <scarolan@gmail.com>
2016-12-17 15:48:20 -0600

    Adding .delivery directory and build_cookbook to bjc-ecommerce.

--- cookbooks/bjc_workstation/metadata.rb
+++ cookbooks/bjc_workstation/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc_workstation'
 long_description 'Installs/Configures bjc_workstation'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.36'
+version '0.1.37'
 
 depends 'workstation'
 depends 'windows'
