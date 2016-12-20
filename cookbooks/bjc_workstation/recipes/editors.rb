c840d3a330d87bcedaee1986c67d3d585b922c10
Nathan Weddle <nweddle@chef.io>
2016-12-19 17:03:05 -0800

    Disable Atom and VSCode Auto Updates

--- cookbooks/bjc_workstation/recipes/editors.rb
+++ cookbooks/bjc_workstation/recipes/editors.rb
@@ -4,6 +4,25 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+home = Dir.home
+
 chocolatey_package 'VisualStudioCode' do
   action :install
+end
+
+# Disable Visual Studio Code Updates
+# https://code.visualstudio.com/docs/supporting/faq#_how-do-i-opt-out-of-vs-code-autoupdates
+directory "#{home}/AppData/Roaming/Code/User"
+
+cookbook_file "#{home}/AppData/Roaming/Code/User/settings.json" do
+  source 'vscode.settings.json'
+  action :create
+end
+
+# Disable Atom Updates
+directory "#{home}/.atom"
+
+cookbook_file "#{home}/.atom/config.cson" do
+  source 'atom.config.cson'
+  action :create
 end
