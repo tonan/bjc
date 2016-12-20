5143bc60d666f9649f34b5c4ddf923b8f6baa48a
Sean Carolan <scarolan@gmail.com>
2016-12-20 14:26:45 -0600

    JSON is now published to S3, and build_demo.sh is updated to use those JSON templates directly.

--- bin/build_demo.sh
+++ bin/build_demo.sh
@@ -2,7 +2,7 @@
 # Builds Chef Demo environment with a Cloudformation template
 
 # Usage and help
-USAGE="Usage: $0 [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department]\n\nExample:\n$0 1.0.4 'Paper Street' \$AWS_KEYPAIR_NAME 4 'Tyler Durden' 'Sales'"
+USAGE="Usage: $0 [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department]\n\nExample:\n$0 1.2.0 'Paper Street' \$AWS_KEYPAIR_NAME 4 'Tyler Durden' 'Sales'"
 
 if [[ $# -ne 6 ]]; then
   echo -e $USAGE
@@ -26,6 +26,5 @@ aws cloudformation create-stack \
 --capabilities CAPABILITY_IAM \
 --region $REGION \
 --tags Key=TTL,Value=${TTL} Key=X-Contact,Value="${CONTACT}" Key=X-Dept,Value="${DEPARTMENT}" Key=X-Project,Value="${CUSTOMER}" Key=X-Termination-Date,Value=${TERMINATION_DATE} \
---template-body file://stacks/bjc-demo-${VERSION}.json \
+--template-url https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-${VERSION}.json \
 --parameters ParameterKey=KeyName,ParameterValue=${SSH_KEY} ParameterKey=TTL,ParameterValue=${TTL}
-
---
+++ stacks/README.md
@@ -0,0 +1,10 @@
+Published Cloudformation templates are now stored in S3.
+Access them either through the AWS console or directly:
+
+AWS console (must be logged in to view)
+https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=bjcpublic&prefix=cloudformation/
+
+Direct URLs:
+https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-1.0.7.json
+https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-1.2.0.json
+etc.
