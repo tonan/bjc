1cc40ee1a633b9e500c75fffdfad28d3c09182c5
Bakh Inamov <b@chef.io>
2017-02-15 17:40:08 -0800

    enables wombat update and deploy with -c azure
    silent feature. needs a yet unreleased build of wombat from master to work.

--- cookbooks/bjc_workstation/recipes/kitchen_templates.rb
+++ cookbooks/bjc_workstation/recipes/kitchen_templates.rb
@@ -3,28 +3,31 @@
 # Recipe:: kitchen_templates
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
-tk_dir = 'C:\Users\Administrator\Desktop\Test_Kitchen'
+
+home = Dir.home
+
+tk_dir = "#{home}/Desktop/Test_Kitchen"
 
 directory tk_dir
 
-cookbook_file "#{tk_dir}\\kitchen_linux.yml" do
+cookbook_file "#{tk_dir}/kitchen_linux.yml" do
   source 'kitchen_linux.yml'
 end
 
-cookbook_file "#{tk_dir}\\kitchen_windows.yml" do
+cookbook_file "#{tk_dir}/kitchen_windows.yml" do
   source 'kitchen_windows.yml'
 end
 
-cookbook_file 'C:\Users\Administrator\user_data' do
+cookbook_file "#{home}/user_data" do
   source 'user_data'
 end
 
 # OS-Specific user_data files to add the automate server to instances' hosts files.
 # Required to send TK data to Visibility.
-cookbook_file 'C:\Users\Administrator\ubuntu_user_data' do
+cookbook_file "#{home}/ubuntu_user_data" do
   source 'ubuntu_user_data'
 end
 
-cookbook_file 'C:\Users\Administrator\windows_user_data' do
+cookbook_file "#{home}/windows_user_data" do
   source 'windows_user_data'
 end
