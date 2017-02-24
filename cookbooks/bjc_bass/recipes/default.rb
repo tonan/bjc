f122add1382cb7d261dc5a8700c204b7814b3e33
Nathan Weddle <nweddle@chef.io>
2017-02-24 14:17:52 -0800

    Remove inspec gem install
    This is now handled by the audit cookbook
    
    Signed-off-by: Nathan Weddle <nweddle@chef.io>

--- cookbooks/bjc_bass/recipes/default.rb
+++ cookbooks/bjc_bass/recipes/default.rb
@@ -4,10 +4,5 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-chef_gem 'inspec' do
-  compile_time true
-  version '0.33.2'
-end
-
 include_recipe 'push-jobs'
 include_recipe 'audit'
