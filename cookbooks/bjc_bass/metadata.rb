f2768093b25c80a345924447cf90c97c35b365fe
Nathan Weddle <nweddle@chef.io>
2017-02-24 14:18:08 -0800

    Update bjc_bass to use current audit and push_jobs cookbooks
    Signed-off-by: Nathan Weddle <nweddle@chef.io>

--- cookbooks/bjc_bass/metadata.rb
+++ cookbooks/bjc_bass/metadata.rb
@@ -6,7 +6,7 @@ source_url 'https://github.com/chef-cft/bjc'
 license 'all_rights'
 description 'Installs/Configures bjc_bass'
 long_description 'Installs/Configures bjc_bass'
-version '0.1.8'
+version '0.2.0'
 
 depends 'push-jobs'
 depends 'audit'
