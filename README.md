e530c71336e8361387a6c539a0dc427dd4d44bd5
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-03-29 16:16:19 +0100

    Refactored demo script to handle Azure Updated the README file on how to use the script and deploy to Azure.
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # Welcome to Project BJC
 ![Magic!](http://i.imgur.com/hknf3Wx.jpg)
 
-Here you will find instructions on how to spin up a standard Chef Demo environment in AWS, as well as instructions on how you can contribute to demo development.  This document assumes you have basic familiarity with AWS, Cloudformation, and SSH keys.  This project is maintained by the Solutions Architects team at Chef.  Issues, pull requests and general feedback are all welcome.  You may email us at saleseng [at] chef.io if you want to get in touch.
+Here you will find instructions on how to spin up a standard Chef Demo environment in AWS or Azure, as well as instructions on how you can contribute to demo development.  This document assumes you have basic familiarity with AWS (Cloudformation and SSH keys) or Azure (ARM Templates and Resource Groups).  This project is maintained by the Solutions Architects team at Chef.  Issues, pull requests and general feedback are all welcome.  You may email us at saleseng [at] chef.io if you want to get in touch.
 
 The talk track script for the standard demo is [located here](https://github.com/chef-cft/bjc/blob/master/SSL_Demo_Script.md)
 
@@ -12,12 +12,25 @@ The talk track script for the standard demo is [located here](https://github.com
 BJC stands for Blue Jean Committee. It's also the code name for the Chef Demo project.
 
 ---
+## Requirements
+---
+Depending on the Cloud Platform that is being deployed to, the relevant command line tools must be installed:
+
+ - **AWS** - https://aws.amazon.com/cli/
+ - **Azure** - https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
+
+---
 ## How do I spin up a demo?
 ---
 #### First, setup your environment:
 1. Clone this repository: `git clone https://github.com/chef-cft/bjc`
 2. Change into the bjc directory: `cd bjc`
-3. Set environment variables for your AWS SSH key name and path, like so.  This must match one of the authorized ec2 ssh keys in your AWS account.
+
+You need to configure the environment for AWS or Azure depending on what you want to deploy to.
+
+##### AWS Credentials
+
+Set environment variables for your AWS SSH key name and path, like so.  This must match one of the authorized ec2 ssh keys in your AWS account.
     * Put these lines into your ~/.bashrc or ~/.zshrc if you want to make them permanent.
 
    ```bash
@@ -25,7 +38,29 @@ BJC stands for Blue Jean Committee. It's also the code name for the Chef Demo pr
    export EC2_SSH_KEY_PATH=~/.ssh/binamov-sa.pem
    ```
 
+##### Azure Credentials
+
+In order to deploy to Azure you need to be logged into the subscription that the will be deployed to.
+
+```bash
+az login
+```
+
+Check that the correct subscription has been selected for deployment.
+
+```bash
+az account list -o table
+```
+
+Ensure that the subscription to be used has the `True` flag oin the `IsDefault` column.  If not then set the correct subscription, for example:
+
+```bash
+az account set --subscription <NAME OR ID OF SUBSCRIPTION>
+```
+
 #### Next, follow these steps to spin up your own dev/test environment:
+
+##### AWS
 The demo environment will provision in AWS fairly quickly, usually within a few minutes.  Once the environment is up there is a startup script you must run to prep the demo.  This script can take 10 minutes or more to complete.  Be sure to give yourself plenty of time prior to the start of your demo for the environment to spin up and for the startup script to run to completion.  We generally recommend setting up at least 30 minutes before your demo to ensure you have enough time.
 
 1.  `git pull` to fetch the latest changes.
@@ -34,7 +69,7 @@ The demo environment will provision in AWS fairly quickly, usually within a few 
     * Replace the demo version and other variables with your own settings:
 
   ```bash
-  ./bin/build_demo.sh 0.2.1 RobCo scarolan_sa 4 'Sean Carolan' 'Solutions Architects'
+  ./bin/build_demo.sh -k scarolan_sa 0.2.1 RobCo 4 'Sean Carolan' 'Solutions Architects'
   ```
 
 3.  Log onto your stack's workstation
@@ -47,4 +82,45 @@ The demo environment will provision in AWS fairly quickly, usually within a few 
 5.  Run the "start me up" script on the desktop and get your demo on!
     * This is the script that can take 10+ minutes to complete.
     * Report any issues you find here:  [https://waffle.io/chef-cft/bjc](https://waffle.io/chef-cft/bjc)
+
+##### Azure
+The demo environment will spin from custom images that have been created in Azure.  The deployment will take about 30 minutes or so, but there should be no additional setup required after this initial run.  As with the AWS demo it is a good idea to ensure that this is done well in advance and for Azure we recommend allowing at least 60 minutes before the demo to ensure you have enough time.
+
+1. `git pull` to fetch the latest changes
+2. Use the `build_demo.sh` script in the `/bin` directory to stand up the stack in the `westus` region by default.
+    * An example command is shown below
+    * Ensure that the version and other variables are set to the required values
+
+```bash
+./bin/build_demo.sh -p azure 2.0.2 RobCo 4 'Russell Seymour' 'Solution Architects'
+```
+
+3.  Log into your demo's workstation
+    * The IP is listed in the outputs of the deployment in the Portal when looking at the Resource Group
+    * Workstation credentials are pinned in #chef-demo-project slack channel.  
+    * If you are not a Chef employee please contact saleseng@chef.io to get the username and password.
+
+## `build_demo.sh` Script
+
+The script now caters for deploying to Azure so there are some changes as to the way in which it is called.
+
+The signature of the command is now:
+
+```bash
+bin/build_demo.sh [-plrkDuh] DEMO_VERSION CUSTOMER_NAME HOURS_DEMO_RUNNING CONTACT DEPARTMENT
+```
+
+The valid options for the command are:
+
+| Switch | Description | Default | Comments |
+|--------|-------------|---------|----------|
+| -p --provider | Select the Cloud Provider to deploy to | aws | |
+| -r --region | Region or location to deploy to | AWS: us-west-2.  Azure: westus | |
+| -l --location | Alias for `-r` | | |
+| -k --key | AWS Key pair to use | | This is a required option for AWS.  (This used to be an argument) |
+| -D --dryrun | Perform a dryrun to see the command(s) that will be executed | false | |
+| -u --url | Base URL from which the JSON template should be retrieved | https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation | |
+| -h --help | Display the help text for the script | | |
+
+The script will check that the necessary command line tools are in installed when deploying to the specified provider.
 ---
--- bin/build_demo.sh
+++ bin/build_demo.sh
@@ -1,30 +1,223 @@
 #!/bin/bash
 # Builds Chef Demo environment with a Cloudformation template
 
-# Usage and help
-USAGE="Usage: $0 [demo-version] [customer name or description] [your aws key name] [hours to keep the demo up] [your name] [your department]\n\nExample:\n$0 1.2.0 'Paper Street' \$AWS_KEYPAIR_NAME 4 'Tyler Durden' 'Sales'"
+# VARIABLES -------------------------------------------
+# Default variables that will be used in the script
+# Cloud Platform to deploy to
+CLOUD_PROVIDER="aws"
 
-if [[ $# -ne 6 ]]; then
-  echo -e $USAGE
-  exit 1
+# Key pair to use in AWS
+SSH_KEY=""
+
+# Base URl to use which holds the necessary templates
+BASE_URL="https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation"
+
+# Ensure the parameters array is empty
+PARAMETERS=()
+ARG_COUNT=0
+DRY_RUN=0
+
+# ----------------------------------------------------
+
+# FUNCTIONS ------------------------------------------
+
+function usage()
+{
+
+  COMMAND=$1
+
+  # create help text to display the usage
+  help_text="$(cat <<EOF
+  Usage: ${COMMAND} [-plrkDuh] DEMO_VERSION CUSTOMER_NAME HOURS_DEMO_RUNNING CONTACT DEPARTMENT
+
+  Options:
+     -k --key       - SSH Key Pair to use in AWS
+     -p --platform  - Cloud platform to use.  Either 'aws' or 'azure'.  Default: aws
+     -r --region    - Location or region to deploy to.  Defaults:  AWS = us-west-2, AZURE = westus
+     -l --location  - Alias for -r switch
+     -D --dryrun    - Dry run to check that the command that will be run
+     -u --url       - Base URL from which to get the template. Default: ${BASE_URL}
+     -h --help      - Display this help
+
+  Options MUST be specified before the required arguments
+EOF
+)"
+
+  echo "$help_text"
+}
+
+function executeCmd()
+{
+  localcmd=$1
+
+  # If Dry Run has been specified then just output the command
+  if [ $DRY_RUN -eq 1 ]
+  then
+    echo $localcmd
+  else
+
+    # Execute the command
+    eval $localcmd
+  fi
+
+}
+
+# ----------------------------------------------------
+
+# Configure the options for the script
+while [[ $# -gt 0 ]]
+do
+
+  key="$1"
+
+  case $key in 
+
+    # Select the cloud platform
+    -p|--provider)
+      shift
+      CLOUD_PROVIDER=`echo $1 | tr '[:upper:]' '[:lower:]'`
+    ;;
+
+    # Specify the location to deploy into
+    -l|--location)
+      shift
+      REGION=$1
+    ;;
+
+    -r|--region)
+      shift
+      REGION=$1
+    ;;
+
+    -h|--help)
+      usage $0
+      exit 1
+    ;;
+
+    -k|--key)
+      shift
+      SSH_KEY=$1
+    ;;
+
+    -D|--dryrun)
+      DRY_RUN=1
+    ;;
+
+    # Any extra parameters that have been set assign to the correct variables
+    *)
+
+      PARAMETERS[ARG_COUNT]="$1"
+
+      let ARG_COUNT++
+
+    ;;
+  esac
+
+  shift
+done
+
+# Create the necessary variables from the PARAMETERS array
+VERSION=${PARAMETERS[0]}
+CUSTOMER=${PARAMETERS[1]}
+CUSTOMER="${CUSTOMER// /-}"
+TTL=${PARAMETERS[2]}
+CONTACT="${PARAMETERS[3]}"
+DEPARTMENT=${PARAMETERS[4]}
+
+# Ensure that all the necessary information has been provided
+if [ "X$VERSION" == "X" ] ||
+   [  "X$CUSTOMER" == "X" ] ||
+   [  "X$TTL" == "X" ] ||
+   [  "X$CONTACT" == "X" ] ||
+   [  "X$DEPARTMENT" == "X" ]
+then
+
+  echo -e "Some required parameters are missing.  Please ensure that all are provided.\n\n"
+  usage $0
+
+  exit 2
 fi
 
-# Variables
-VERSION=$1
-CUSTOMER=$2
-CUSTOMER="${CUSTOMER// /-}"
-SSH_KEY=$3
-TTL=$4
-CONTACT=$5
-DEPARTMENT=$6
-TERMINATION_DATE="$(TZ=Etc/UTC date -j -v +$4H +'%Y-%m-%dT%H:%M:%SZ')"
-REGION=us-west-2
+# Determine the termination date
+TERMINATION_DATE="$(TZ=Etc/UTC date -j -v +${TTL}H +'%Y-%m-%dT%H:%M:%SZ')"
 
-# Here's where we create the stack
-aws cloudformation create-stack \
---stack-name "${USER}-${CUSTOMER}-Chef-Demo-$(TZ=Etc/UTC date +'%Y%m%dT%H%M%SZ')" \
---capabilities CAPABILITY_IAM \
---region $REGION \
---tags Key=TTL,Value=${TTL} Key=X-Contact,Value="${CONTACT}" Key=X-Dept,Value="${DEPARTMENT}" Key=X-Project,Value="${CUSTOMER}" Key=X-Termination-Date,Value=${TERMINATION_DATE} \
---template-url https://s3-us-west-2.amazonaws.com/bjcpublic/cloudformation/bjc-demo-${VERSION}.json \
---parameters ParameterKey=KeyName,ParameterValue=${SSH_KEY} ParameterKey=TTL,ParameterValue=${TTL}
+# Determine the name of the stack
+STACK_NAME=$(printf "%s-%s-Chef-Demo-$(TZ=Etc/UTC date +'%Y%m%dT%H%M%SZ')" ${CONTACT// /-} ${CUSTOMER})
+
+# Based on the cloud provider set the region default
+case $CLOUD_PROVIDER in
+  aws)
+    REGION="${REGION:-us-west-2}"
+
+    # Ensure that the SSH_KEY has been specified
+    if [ "X$SSH_KEY" == "X" ]
+    then
+      echo -e "An SSH Key Pair must been specified using the -k option when deploying to AWS\n\n"
+      usage $0
+      exit 3
+    fi
+
+    # Ensure that the aws command is available
+    if ! hash aws 2>/dev/null
+    then
+      echo "Please ensure that the AWS command line tools are installed"
+
+      # only exit if this is not a dry run
+      if [ $DRY_RUN -eq 0 ]
+      then
+        exit 4
+      fi
+    fi
+
+    # Build up the command to run to deploy the cloud formation template
+    cmdparts=()
+    cmdparts[0]=$(printf 'aws cloudformation create-stack')
+    cmdparts[1]=$(printf ' --stack-name "%s"' $STACK_NAME)
+    cmdparts[2]=$(printf ' --region %s', $REGION)
+    cmdparts[3]=$(printf ' --tags Key=TTL,Value=%s Key=X-Contact,Value="%s" Key=X-Dept,Value="%s" Key=X-Project,Value="%s" Key=X-Termination-Date,Value="%s"' $TTL "$CONTACT" "$DEPARTMENT" "$CUSTOMER" $TERMINATION_DATE)
+    cmdparts[4]=$(printf ' --template-url %s/bjc-demo-aws-%s.json' $BASE_URL $VERSION)
+    cmdparts[5]=$(printf ' --parameters ParameterKey=KeyName,ParameterValue=%s ParameterKey=TTL,ParameterValue=%s' $SSH_KEY $TTL)
+    
+    # Join the cmdparts together to get the overall command
+    cmd="${cmdparts[@]}"
+
+    executeCmd "$cmd"
+
+  ;;
+  azure)
+    REGION="${REGION:-westus}"
+
+    # Ensure that the az command is installed
+    if ! hash az 2>/dev/null
+    then
+      echo "Please ensure that Azure CLI 2.0 command line tools are installed"
+      echo -e "   https://docs.microsoft.com/en-us/cli/azure/install-azure-cli\n\n"
+
+      # only exit if this is not a dry run
+      if [ $DRY_RUN -eq 0 ]
+      then
+        exit 4
+      fi
+    fi
+
+    # Build up the command to create the necessary resource group for the deployment
+    cmdparts=()
+    cmdparts[0]=$(printf 'az group create')
+    cmdparts[1]=$(printf ' -l "%s"' $REGION)
+    cmdparts[2]=$(printf ' -n "%s"' $STACK_NAME)
+    cmdparts[3]=$(printf ' --tags TTL=%s X-Contact="%s" X-Dept="%s" X-Project="%s" X-Termination-Date="%s"' $TTL "$CONTACT" "$DEPARTMENT" "$CUSTOMER" $TERMINATION_DATE)
+
+    cmd="${cmdparts[@]}"
+    executeCmd "$cmd"
+
+    # Create the command to perform the deployment
+    cmdparts=()
+    cmdparts[0]=$(printf 'az group deployment create')
+    cmdparts[1]=$(printf ' -g "%s"' $STACK_NAME)
+    cmdparts[2]=$(printf ' --template-uri %s/bjc-demo-azure-%s.json' $BASE_URL $VERSION)
+
+    cmd="${cmdparts[@]}"
+    executeCmd "$cmd"
+  ;;
+esac
+
