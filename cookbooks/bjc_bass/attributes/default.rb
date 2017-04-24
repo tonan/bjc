dd20320b2093e7289528dfe26d98945da129cf12
Sean Carolan <scarolan@gmail.com>
2017-04-24 10:38:42 -0500

    Revert SSL baseline profile to last known working, v1.1.1

--- cookbooks/bjc_bass/attributes/default.rb
+++ cookbooks/bjc_bass/attributes/default.rb
@@ -3,6 +3,6 @@ default['audit']['collector'] = 'chef-visibility'
 default['audit']['profiles'] = [
   {
     name: 'admin/ssl-benchmark',
-    url: 'https://github.com/dev-sec/ssl-baseline/archive/v1.2.0.tar.gz'
+    url: 'https://github.com/dev-sec/ssl-baseline/archive/v1.1.1.tar.gz'
   }
 ]
