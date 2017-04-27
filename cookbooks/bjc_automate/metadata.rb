de3145848eedd83844b1756cf82f54e0cfac1812
Bakh Inamov <b@chef.io>
2017-04-27 08:30:22 -0700

    revert to chef 12

--- cookbooks/bjc_automate/metadata.rb
+++ cookbooks/bjc_automate/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc-automate'
 long_description 'Installs/Configures bjc-automate'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.30'
+version '0.1.31'
 
 depends 'wombat'
 depends 'automate'
