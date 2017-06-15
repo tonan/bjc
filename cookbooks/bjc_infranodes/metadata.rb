4e17b1b423fccc4f54437a7ff7658e30ed614c27
Bakh Inamov <b@chef.io>
2017-06-15 15:39:56 -0700

    fixes knife.rb to give relative paths to client_key

--- cookbooks/bjc_infranodes/metadata.rb
+++ cookbooks/bjc_infranodes/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc_infranodes'
 long_description 'Installs/Configures bjc_infranodes'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.23'
+version '0.1.24'
 
 depends 'infranodes'
 depends 'wombat'
