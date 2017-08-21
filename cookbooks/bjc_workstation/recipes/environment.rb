8f27593003b1300427ed94cefd745787340a1196
Nick Rycar <rycar@chef.io>
2017-08-21 15:16:07 -0700

    Update VSC Aliases

--- cookbooks/bjc_workstation/recipes/environment.rb
+++ cookbooks/bjc_workstation/recipes/environment.rb
@@ -7,7 +7,8 @@ end
 
 [ "#{run_home}\\AppData\\Local\\atom\\bin",
   'C:\tools\cmder',
-  'C:\Program Files (x86)\Microsoft VS Code\bin'].each do |p|
+  'C:\Program Files (x86)\Microsoft VS Code\bin',
+  'C:\Program Files\Microsoft VS Code\bin'].each do |p|
   windows_path p do
     action :add
   end
