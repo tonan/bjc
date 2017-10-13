d7ea45704ed9023d2cf2f372a75fb7373a444550
Nathan Weddle <nweddle@chef.io>
2017-10-13 12:02:40 -0700

    Build it again, Sam!

--- cookbooks/bjc_infranodes/metadata.rb
+++ cookbooks/bjc_infranodes/metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures bjc_infranodes'
 long_description 'Installs/Configures bjc_infranodes'
 issues_url 'https://waffle.io/chef-cft/bjc'
 source_url 'https://github.com/chef-cft/bjc'
-version '0.1.26'
+version '0.1.27'
 
 depends 'infranodes'
 depends 'wombat'
