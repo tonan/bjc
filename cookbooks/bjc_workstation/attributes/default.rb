33516d2f6e726e143852120e8d4536b82547954e
Nick Rycar <rycar@chef.io>
2017-12-05 11:53:46 -0800

    Using %w array to make linting happy.

--- cookbooks/bjc_workstation/attributes/default.rb
+++ cookbooks/bjc_workstation/attributes/default.rb
@@ -1,2 +1,2 @@
 default['bjc_workstation']['cookbooks'] = ['bjc-ecommerce', 'bjc_bass', 'dca_demo']
-default['bjc_workstation']['profiles'] = ['apache_webserver', 'linux_baseline_wrapper']
+default['bjc_workstation']['profiles'] = %w(apache_webserver linux_baseline_wrapper)
